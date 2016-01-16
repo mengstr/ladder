e374f0b714913a991b416bf8864c298de60a76ea
matseng <mats.engstrom@gmail.com>
2016-01-16 15:00:21 +0800

    Rudimentery key- and framerate handling

--- levels.go
+++ levels.go
@@ -1,5 +1,19 @@
 package main
 
+import "errors"
+
+// p - The place where the lad starts.
+// V - Der Dispenser. Der Rocks roll out of it to squash you flat.
+// * - Der Eaters. They eat the Der Rocks but oddly do not harm you in the slightest
+// = - Floor. You walk on it.
+// H - Ladder. You climb it.
+// | - Wall. You can't walk through it. You're not a ghost....yet.
+// . - Rubber Ball. It's very bouncy. This difference is, it bounces you.
+// $ - Treasure. The lad must get here to finish the level.
+// & - Gold Statue. Money!Money!Money!Money!Money!
+// ^ - Fire. Turns you into extra crispy bacon.
+// - - Disposable Floor. Well, you can walk on it once.
+
 var levels = []struct {
   name   string
   layout []string
@@ -179,3 +193,59 @@ var levels = []struct {
     },
   },
 }
+
+type Coords struct {
+  x int
+  y int
+}
+
+type MapData struct {
+  lad           Coords
+  dispensers    []Coords
+  ladsRemaining int
+  level         int
+  score         int
+  bonustime     int
+  field         [20][79]byte
+}
+
+//
+//
+func loadMap(n int) (MapData, error) {
+  if n > len(levels) {
+    return MapData{}, errors.New("Level out of range")
+  }
+  var i, j int
+  var m MapData
+
+  for i = 0; i < 20; i++ {
+    for j = 0; j < 79; j++ {
+      m.field[i][j] = '.'
+    }
+  }
+
+  for i = 0; i < len(levels[n].layout); i++ {
+    //    fmt.Println(levels[n].layout[i])
+    for j = 0; j < len(levels[n].layout[i]); j++ {
+      switch levels[n].layout[i][j] {
+      case 'p':
+        m.field[i][j] = 'p'
+        m.lad = Coords{i, j}
+      case 'V':
+        m.field[i][j] = 'V'
+        m.dispensers = append(m.dispensers, Coords{i, j})
+      case '.': // TODO - handle the rubber balls
+        m.field[i][j] = '.'
+      default:
+        m.field[i][j] = levels[n].layout[i][j]
+      }
+    }
+  }
+
+  return m, nil
+}
+
+// p - The place where the lad starts.
+// V - Der Dispenser. Der Rocks roll out of it to squash you flat.
+// * - Der Eaters. They eat the Der Rocks but oddly do not harm you in the slightest
+// . - Rubber Ball. It's very bouncy. This difference is, it bounces you.
--- main.go
+++ main.go
@@ -1,10 +1,112 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nsf/termbox-go"
+)
+
+const screenMinWidth = 80
+const screenMinHeight = 22
+
+var m MapData
 
 func main() {
-	fmt.Println("\033[2J")
-	// fmt.Println("\033[?5h")
-	fmt.Println("Name:", levels[1].name)
-	fmt.Println("Bottom", levels[1].layout[18])
+
+	// Initialize termbox library
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	// Check that the screen is big enough for us
+	width, height := termbox.Size()
+	if width < screenMinWidth || height < screenMinHeight {
+		termbox.Close()
+		log.Fatal("Error - screen W/H ", width, "/", height, " is less than ", screenMinWidth, "/", screenMinHeight)
+	}
+	termbox.SetOutputMode(termbox.OutputNormal)
+
+	m, _ = loadMap(0)
+	for i := 0; i < 20; i++ {
+		for j := 0; j < 79; j++ {
+			//			fmt.Printf("%c", m.field[i][j])
+		}
+		fmt.Println()
+	}
+	//	fmt.Println("Lad is at ", m.lad)
+	//	fmt.Println("Dispensers at ", m.dispensers)
+
+	drawMap()
+	termbox.Flush()
+
+	// Make termbox generate events on a channel for keypresses
+	keystroke := make(chan termbox.Event)
+	go func() {
+		for {
+			keystroke <- termbox.PollEvent()
+		}
+	}()
+
+	// Set up a channel for the game frame rate
+	ticker := time.NewTicker(200 * time.Millisecond)
+
+gameloop:
+	for {
+		select {
+		case <-ticker.C:
+			m.score++
+			drawStatusline()
+			termbox.Flush()
+		case ev := <-keystroke:
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyEsc {
+				break gameloop
+			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyArrowLeft {
+			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyArrowRight {
+			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyArrowUp {
+			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeyArrowDown {
+			}
+			if ev.Type == termbox.EventKey && ev.Key == termbox.KeySpace {
+			}
+		}
+
+	}
+
+}
+
+//
+//
+//
+func drawMap() {
+	for y := 0; y < len(m.field); y++ {
+		for x := 0; x < len(m.field[0]); x++ {
+			termbox.SetCell(x, y, rune(m.field[y][x]), termbox.ColorDefault, termbox.ColorDefault)
+		}
+	}
+	drawStatusline()
+}
+
+//
+//
+//
+func drawStatusline() {
+	//	Lads    5     Level    1     Score     000                 Bonus time    3400
+	status := fmt.Sprintf("Lads   %2d     Level   %2d     Score    %04d                 Bonus time    %4d", m.ladsRemaining, m.level, m.score, m.bonustime)
+	printXY(0, 20, termbox.ColorDefault, termbox.ColorDefault, status)
+}
+
+//
+//
+//
+func printXY(x int, y int, fgcolor termbox.Attribute, bgcolor termbox.Attribute, txt string) {
+	for i, elem := range txt {
+		termbox.SetCell(x+i, y, rune(elem), fgcolor, bgcolor)
+	}
 }
