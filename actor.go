1a1e35376831d94edc7efd423c0829ab807dc89d
matseng <mats.engstrom@gmail.com>
2016-01-20 17:55:05 +0800

    Support for jumps

--- actor.go
+++ actor.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate stringer -type=Action
+
 // The Actor struct holds info about an actor I.E the Lad or a Rock
 type Actor struct {
 	Type       int
@@ -8,20 +10,69 @@ type Actor struct {
 	Ch         byte
 	Dir        Action
 	DirRequest Action
+	JumpStep   int
 }
 
-// The Action constans define what the Actor currently is, or is requested to be, doing
 type Action int
 
+// The Action constans define what the Actor currently is, or is requested to be, doing
 const (
 	STOPPED Action = iota
-	LEFT
-	RIGHT
 	UP
+	UPRIGHT
+	RIGHT
+	DOWNRIGHT
 	DOWN
+	DOWNLEFT
+	LEFT
+	UPLEFT
 	FALLING
-	JUMP
+	JUMP // Generic jump set by keyhandler
+	JUMPRIGHT
+	JUMPUP
+	JUMPLEFT
 )
+
+var jumpLeft = []Action{UPLEFT, UPLEFT, LEFT, LEFT, DOWNLEFT, DOWNLEFT}
+var jumpRight = []Action{UPRIGHT, UPRIGHT, RIGHT, RIGHT, DOWNRIGHT, DOWNRIGHT}
+var jumpUp = []Action{UP, UP, STOPPED, DOWN, DOWN}
+
+var jumpPaths = map[Action][]Action{
+	JUMPUP:    jumpUp,
+	JUMPLEFT:  jumpLeft,
+	JUMPRIGHT: jumpRight,
+}
+
+var dirs = map[Action]struct {
+	x, y int
+}{
+	STOPPED:   {0, 0},
+	UP:        {0, -1},
+	UPRIGHT:   {1, -1},
+	RIGHT:     {1, 0},
+	DOWNRIGHT: {1, 1},
+	DOWN:      {0, 1},
+	DOWNLEFT:  {-1, 1},
+	LEFT:      {-1, 0},
+	UPLEFT:    {-1, -1},
+	FALLING:   {0, 1},
+	JUMP:      {0, 0},
+	JUMPRIGHT: {0, 0},
+	JUMPLEFT:  {0, 0},
+	JUMPUP:    {0, 0},
+}
+
+//
+// A moving jummp is UR/UR/R/R/DR/DR
+// or                UL/UL/L/L/DL/DL
+// A standing jump is U/U/-/D/D
+//
+// 	 ====================
+//   ----234-----23------
+//   ---1---5----14------
+//   --0-----6---05------
+//   ====================
+//
 
 //
 //
@@ -31,11 +82,47 @@ func MoveActor(a Actor, m MapData) Actor {
 loopAgain: // If just started falling we need to retest all conditions
 
 	// If stopped or going left or going right and requst to do something else, then try to do it
-	if (a.Dir == STOPPED && a.DirRequest != STOPPED) ||
+	if (a.Dir == STOPPED && a.DirRequest == LEFT) ||
+		(a.Dir == STOPPED && a.DirRequest == RIGHT) ||
 		(a.Dir == LEFT && a.DirRequest == RIGHT) ||
 		(a.Dir == RIGHT && a.DirRequest == LEFT) {
 		a.Dir = a.DirRequest
 		a.DirRequest = STOPPED
+
+	}
+
+	// Handle starting of jumps
+	if a.DirRequest == JUMP {
+		switch a.Dir {
+		case STOPPED:
+			a.DirRequest = a.Dir
+			a.Dir = JUMPUP
+			a.JumpStep = 0
+		case LEFT:
+			a.DirRequest = a.Dir
+			a.Dir = JUMPLEFT
+			a.JumpStep = 0
+		case RIGHT:
+			a.DirRequest = a.Dir
+			a.Dir = JUMPRIGHT
+			a.JumpStep = 0
+		}
+	}
+
+	// Do the jumping
+	if a.Dir == JUMPUP || a.Dir == JUMPLEFT || a.Dir == JUMPRIGHT {
+		jd := jumpPaths[a.Dir][a.JumpStep]
+		if m.Field[a.Y+dirs[jd].y][a.X+dirs[jd].x] == ' ' {
+			a.X += dirs[jd].x
+			a.Y += dirs[jd].y
+			a.JumpStep++
+			if a.JumpStep >= len(jumpPaths[a.Dir]) {
+				a.Dir = a.DirRequest
+			}
+		} else {
+			// If bumped into something try falling
+			a.Dir = FALLING
+		}
 	}
 
 	// If at the edige of the playfield stop and exit
